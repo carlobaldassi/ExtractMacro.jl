92f41ee5b19e0c71a33c6d406b900295aac0417f
Carlo Baldassi <carlobaldassi@gmail.com>
2016-07-13 22:44:24 +0200

    Coverage from julia-release

--- docs/coverage.jl
+++ docs/coverage.jl
@@ -1,6 +1,6 @@
 # Only run coverage from linux nightly build on travis.
 get(ENV, "TRAVIS_OS_NAME", "")       == "linux"   || exit()
-get(ENV, "TRAVIS_JULIA_VERSION", "") == "nightly" || exit()
+get(ENV, "TRAVIS_JULIA_VERSION", "") == "release" || exit()
 
 using Coverage
 
