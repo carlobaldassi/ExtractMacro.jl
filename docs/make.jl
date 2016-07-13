a2bb0ddb9428071493296203f8b70902022e715e
Carlo Baldassi <carlobaldassi@gmail.com>
2016-07-13 17:36:43 +0200

    Fix docs/make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -5,6 +5,5 @@ makedocs()
 deploydocs(
     deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
     repo   = "github.com/carlobaldassi/ExtractMacro.jl.git",
-    julia  = "0.4",
-    osname = "linux"
+    julia  = "release"
 )
