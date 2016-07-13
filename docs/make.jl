c9d9bc311c607edf31bdadd7ad4de216de1e0902
Carlo Baldassi <carlobaldassi@gmail.com>
2016-07-13 15:26:24 +0200

    More docs deployment changes

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,10 @@
 using Documenter, ExtractMacro
 
 makedocs()
+
+deploydocs(
+    deps   = Deps.pip("pygments", "mkdocs", "python-markdown-math"),
+    repo   = "github.com/carlobaldassi/ExtractMacro.jl.git",
+    julia  = "0.4",
+    osname = "linux"
+)
